ec71624a681858e5f2ead2393f617b0769a46b09
Tim Quinn <mstrtimespace@gmail.com>
2016-02-07 16:40:04 -0500

    adding updated recipes and new interface recipe

---
+++ cookbooks/quinncloud/recipes/network.rb
@@ -0,0 +1,19 @@
+# Quinncloud cookbook network setup
+# Set up interface to go online at boot
+
+ifconfig eth0 do
+  device 'eth0'
+  onboot true
+  retries '3'
+  bootproto 'dhcp'
+end
+
+cookbook_file '/etc/hosts' do
+  source 'hosts'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
---
+++ cookbooks/quinncloud/recipes/web_site_1.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+# Ruby script to try and start learning rails
+# Don't expect much
+
+# Check the apache cookbook for details on how the server is configured
