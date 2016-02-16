9e7c0f8ff6791f7ddaccd9f7c3a3c35df06e367f
Tim Quinn <mstrtimespace@gmail.com>
2016-02-15 22:42:39 -0500

    a probably unworking version of quinncloud::initial_setup but checking in progress

--- cookbooks/quinncloud/recipes/initial_setup.rb
+++ cookbooks/quinncloud/recipes/initial_setup.rb
@@ -3,7 +3,7 @@
 # from /var/lock
 
 # install packages
-base_packages = "#{node['package_set']}"
+base_packages = "#{node['install_adds']['package_set']}"
 package "install_set" do
   package_name [base_packages]
   not_if base_packages = nil
@@ -44,15 +44,6 @@ cookbook_file "/home/tquinn/.gitconfig" do
   action :create
 end
 
-# Time to use that tar cookbook to install ruby version 2.2
-cookbook_file "/home/tquinn/ruby-2.3.0.tar.gz" do
-  source "ruby-2.3.0.tar.gz"
-  owner "tquinn"
-  group "tquinn"
-  mode "0644"
-  action :create
-end
-
 directory '/opt/ruby' do
   owner 'tquinn'
   group 'tquinn'
@@ -60,3 +51,65 @@ directory '/opt/ruby' do
   action :create
 end
 
+# Upload the ruby tarball to the client
+cookbook_file "/opt/ruby/ruby-2.3.0.tar.gz" do
+  source "ruby-2.3.0.tar.gz"
+  owner "tquinn"
+  group "tquinn"
+  mode "0644"
+  action :create
+end
+
+# Here is the conundrum, do I use a bash block or build a lwrp?  I
+# Will drop a bash block to get it working but this is really a crap
+# solution and i know it...  tq 2/15/16
+
+bash 'extract_build_ruby' do
+  cwd ::File.dirname(/opt/ruby/)
+  code <<-EOH
+    tar xzf ruby-2.3.0.tar.gz
+    cd ruby-2.3.0
+    ./configure --enable-shared
+    make all
+    make test
+    if [$? -ne 0]
+      then
+        make install
+      else
+        echo "trying to make sure the directoy is right and trying one more time"
+        cd /opt/ruby/ruby-2.3.0
+        ./configure --enable-shared
+        make all
+        make test
+        if [?! -ne 0]
+          then
+            echo "giving up, tests broken"
+            exit 20
+          else echo "fix your directory structure!"
+        fi
+    fi
+    echo "Ruby 2.3.0 installed"
+    EOH
+  user "root"
+  group "root"
+  action :run
+end
+
+# Now we have sqlite, ruby 2.3.0, and ruby installed.  Oh snap - gemfile!
+# We are already setting packages in the role...but I think this is more
+# specific to the recipe so lets drop it in attributes.
+
+template "/opt/ruby/.gemrc" do
+  source "Gemfile.erb"
+  only_if node['install_adds']['gem_set']
+  action :create
+  notifies :run, 'ruby_block[refresh_gemrc]', :immediately
+end
+
+ruby_block 'refresh_gemrc' do
+  only_if node['install_adds']['gem_set']
+  action 'nothing'
+  block do
+    Gem.configuration - Gem::ConfigFile.new []
+  end
+end
