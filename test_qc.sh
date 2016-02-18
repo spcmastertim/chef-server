6c6fecfdb6284c3f6a18104451a693bc8dc42b6f
Tim Quinn <mstrtimespace@gmail.com>
2016-02-17 23:16:56 -0500

    built testing tool which can be pushed to all systems and will alert of problems with the testing tools

---
+++ test_qc.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# test_cb.sh - test cookbooks at path $1 using "knife cookbook test",
+#   "foodcritic -t all", and "rubocop" testing suites.  If rspec tests exist
+#   "run those too"
+cookbook_home="/home/tquinn/chef-repo/cookbooks"
+cookbook_name=$1
+
+knife_exe=`which knife`
+foodcritic_exec=`which foodcritic`
+rubocop_exec=`which rubocop`
+
+# Test to see if you forgot to enter a cookbook, if you did ask.
+# Also, if you are too lazy to respond I only ask once!
+if [ -d $cookbook_home/$cookbook_name ]
+then
+  echo "please enter a cookbook name"
+  if [ -z $cookbook_name ]
+  then
+    read -e cookbook_name
+    if [ -d $cookbook_home/$cookbook_name ]
+    export cookbook_name
+    then
+      echo "$cookbook_name will be checked"
+    else
+      echo "later tater"
+      exit 2
+    fi
+  fi
+fi
+
+if [ -z $knife_exe ]
+then
+  echo "you forgot to install knife"
+  exit 2
+else
+  $knife_exe cookbook test $cookbook_name
+fi
+
+if [ -z $foodcritic_exec ]
+then
+  echo "you forgot to install foodcritic"
+  exit 3
+else
+  $foodcritic_exec -t all $cookbook_home/$cookbook_name
+fi
+
+if [ -z $rubocop_exec ]
+then
+  echo "you forgot to install rubocop"
+  exit 4
+else
+  $rubocop_exec $cookbook_home/$cookbook_name
+fi
+
+echo "finished testing!"
