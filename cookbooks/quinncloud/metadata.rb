07be19af336bcce4bd82ba76078133c6a35511f7
Tim Quinn <mstrtimespace@gmail.com>
2016-03-07 12:42:37 -0500

    metadata bump

--- cookbooks/quinncloud/metadata.rb
+++ cookbooks/quinncloud/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'tquinn@quinncloud.com'
 license          'All rights reserved'
 description      'Installs/Configures quinncloud specific software'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.18'
+version          '0.3.19'
 depends          'users'
 depends          'sudo'
